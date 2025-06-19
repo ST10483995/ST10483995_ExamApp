9114a8a2ba97e899acc2e1ddab0f51dccfafa933
ST10483995 <st10483995@vcconnect.edu.za>
2025-06-19 08:21:52 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_examapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package za.co.varsitycollege.st10483995.st10483995_examapp
+//ST10483995 Lipalesa Lethole
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Decla
+        val addButton = findViewById<Button>(R.id.addButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val exitButton = findViewById<Button>(R.id.exitButton)
+
+
+    }
+}
